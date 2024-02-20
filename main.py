d1fc6c2cd88274941685396e2f53545a5a730b1c
JahsonGA <jgonzalezallie@gmail.com>
2024-02-20 16:46:08 -0500

    image processing complete. TODO fine tune

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import cv2
 import pytesseract
 import numpy as np
+import PIL
+#import pandas as pd
 
 print(cv2.__version__)
 print(pytesseract.__version__)
 print(np.__version__)
+print(PIL.__version__)
+#print(pd.__version__)
